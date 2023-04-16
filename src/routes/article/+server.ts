b4649022299dd56dfa512b15b6c2897ae8a44784
yych42 <43839115+yych42@users.noreply.github.com>
2023-04-16 10:21:55 +0200

    Add support for article extraction from html

--- src/routes/article/+server.ts
+++ src/routes/article/+server.ts
@@ -3,9 +3,26 @@ import { Readability } from '@mozilla/readability';
 import { JSDOM } from 'jsdom';
 
 export const POST: RequestHandler = async ({ request }) => {
-	const { url } = await request.json();
-	if (!url) throw error(400, 'No URL provided');
-	const dom = await JSDOM.fromURL(url);
+	const { type, payload } = await request.json();
+	if (!type || !payload) throw error(400, 'No URL or payload provided');
+	console.log(type, payload);
+	if (type !== 'url' && type !== 'html') throw error(400, 'Invalid type');
+
+	let decoded: string;
+	try {
+		decoded = Buffer.from(payload, 'base64').toString('utf-8');
+	} catch (e) {
+		throw error(400, 'Invalid payload');
+	}
+
+	let dom: JSDOM;
+	if (type === 'url') {
+		const url = decoded;
+		dom = await JSDOM.fromURL(url);
+	} else {
+		dom = new JSDOM(decoded);
+	}
+
 	const reader = new Readability(dom.window.document);
 	const article = reader.parse() ?? { textContent: '', title: '', length: 0, lang: '' };
 
